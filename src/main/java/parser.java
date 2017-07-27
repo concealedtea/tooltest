76ed8460bdfd1b1c0d90b287d3849451f2284075
Julius Fan <jzfan@ucsc.edu>
2017-07-27 15:50:41 +0800

    updated log4j2 configuration fixed failed project push onto hdp added platform compatibility with VM

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.*;
 /**
  * Created by rore256 on 7/27/2017.
  * sample line from dataset:
@@ -14,7 +13,7 @@ public class parser {
         int i =0;
         try {
             while (i < 4) {
-                File file = new File("C:/Users/thastq/IdeaProjects/tooltest/data/00000" + i + "_0.txt");
+                File file = new File("C:/Users/thatq/IdeaProjects/tooltest/data/00000" + i + "_0");
                 FileInputStream stream = null;
                 stream = new FileInputStream(file);
                 DataInputStream in = new DataInputStream(stream);
