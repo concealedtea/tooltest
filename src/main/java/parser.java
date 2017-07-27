14cbb803153be24d9da958b608c840121880020b
Julius Fan <jzfan@ucsc.edu>
2017-07-27 15:02:59 +0800

    commit parser

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * Created by rore256 on 7/27/2017.
  * sample line from dataset:
@@ -6,11 +8,13 @@ import java.io.*;
  * total 4 datafiles 000000_0,000001_0,000002_0,000003_0
  */
 public class parser {
+    private static final Logger logger = LogManager.getLogger(parser.class);
+
     public static void main(String args[]) {
         int i =0;
         try {
             while (i < 4) {
-                File file = new File("C:/Users/thatq/IdeaProjects/falcoparse/data/00000" + i + "_0.txt");
+                File file = new File("C:/Users/thastq/IdeaProjects/falcoparse/data/00000" + i + "_0.txt");
                 FileInputStream stream = null;
                 stream = new FileInputStream(file);
                 DataInputStream in = new DataInputStream(stream);
@@ -18,7 +22,7 @@ public class parser {
 
                 String strLine;
                 while ((strLine = br.readLine()) != null) {
-                    System.out.println(strLine);
+                    logger.info(strLine);
                 }
                 in.close();
                 i++;
